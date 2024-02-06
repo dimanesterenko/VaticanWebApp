061af2468b75c3d56aea762089bb6a6fbed3d7b0
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-02-06 15:04:58 +0200

    add 001 migrations

--- Back/Back/settings.py
+++ Back/Back/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'Back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'VaticanMuseumDB',
+        'USER': 'dima',
+        'PASSWORD': '21022004',
     }
 }
 
