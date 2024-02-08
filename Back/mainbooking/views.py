f066e65c3fb163f35d9e3d0be5a822056f9b8067
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-02-08 13:49:47 +0200

    add test-data from admin-panel to DB

--- Back/mainbooking/views.py
+++ Back/mainbooking/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import VisitorForm
-
+from .models import Booking
 def create_visitor(request):
     if request.method == 'POST':
         form = VisitorForm(request.POST)
         if form.is_valid():
-            form.save()
+            visitor = form.save(commit=False)
+            visitor.save()
+            booking_data = {
+                'Visitor_ID': visitor.pk,  # Отримуємо ID збереженого візітора
+                'Responsible_ID': request.POST['responsible_id'],  # Отримуємо ID відповідальної особи з POST запиту
+                'Guide_ID': request.POST['guide_id'],  # Отримуємо ID гіда з POST запиту
+                'Booking_Count': int(request.POST['booking_count']),  # Отримуємо кількість з POST запиту
+                'Booking_Date': request.POST['booking_date'],  # Отримуємо дату з POST запиту
+                'Booking_Time': request.POST['booking_time'],  # Отримуємо час з POST запиту
+            }
+            booking = Booking(**booking_data)  # Створюємо об'єкт бронювання
+            booking.save()  # Зберігаємо бронювання в БД
+
+            # Після успішного збереження можна редіректити або робити інші дії
+            return redirect('success_page')
             # Редірект або інші дії після успішного збереження
     else:
         form = VisitorForm()
