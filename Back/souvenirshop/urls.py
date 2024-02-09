80ec1770288df29e511c04f468c20647af673f83
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-02-10 01:34:21 +0200

    add shop template

--- Back/souvenirshop/urls.py
+++ Back/souvenirshop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SouvenirViewSet
-
+from souvenirshop import views
 router = DefaultRouter()
 router.register(r'souvenirs', SouvenirViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('shop/',views.souvenirs_shop, name='souvenirs_shop')
 ]
--- Back/souvenirshop/views.py
+++ Back/souvenirshop/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Souvenir
@@ -6,3 +7,8 @@ from .serializers import SouvenirSerializer
 class SouvenirViewSet(viewsets.ModelViewSet):
     queryset = Souvenir.objects.all()
     serializer_class = SouvenirSerializer
+
+
+def souvenirs_shop(request):
+    souvenirs = Souvenir.objects.all()
+    return render(request, 'souvenirs.html', {'souvenirs': souvenirs})
